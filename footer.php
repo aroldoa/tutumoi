c6ba53ad07f00bc94916bb1fe986c5126186e76e
Aroldo Alvarez <aroldo@primomotif.com>
2014-03-17 15:43:58 -0500

    Creating html

---
+++ footer.php
@@ -0,0 +1,43 @@
+    <div class="pre-footer">
+	    <div class="col-lg-12">
+
+	    </div>
+    </div>
+	    <div class="clearfix"></div>
+	<footer>
+	    <div class="col-sm-3">
+		    <h5>Customer Support</h5>
+		    <ul>
+				<li><a href="#">About Us</a></li>
+			    <li><a href="#">Shipping & Returns</a></li>
+			    <li><a href="#">Our  Locations</a></li>
+			    <li><a href="#">Help Placing an Order</a></li>
+		    </ul>
+		</div>
+		<div class="col-sm-3">
+		    <h5>Recent Articles</h5>
+		</div>
+		<div class="col-sm-3">
+		    <h5>Instagram Feed</h5>
+		</div>
+		<div class="col-sm-3">
+		    <h5>Join Our Newsletter</h5>
+		</div>
+	 </footer>
+	 <div class="copyright">
+		 <div class="col-lg-10">
+			 &copy; Copyright 2014 Tutumoi, Inc.
+		 </div>
+		 <div class="col-lg-2"></div>
+	 </div>
+
+
+</div><!-- end of Wrap -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+
+    <title>Tutus, Designer Tutus, Fashion Tutus for Little Girls</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+     <link href="style.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+<body>
+<div class="container wrap">
+	<div class="col-lg-12 top-links">
+	<div class="col-lg-5"><p>NEED HELP WITH AN ORDER?  INFO@TUTUMOI.COM</p></div>
+		<div class="col-lg-6 col-lg-offset-1">
+			<ul>
+				<a href="#"><li>Sign In</li></a>
+				<a href="#"><li>My Account</li></a>
+				<a href="#"><li>Customer Service</li></a>
+				<a href="#"><li>Shopping Bag</li></a>
+			</ul>
+		</div>
+	</div><!-- end of Top Links -->
+	<header>
+		<div class="col-lg-12 logoarea">
+			<a href="#"><img src="images/logo.png" alt="Tutus"/></a>
+		</div><!-- end of Logo -->
+		<div class="col-lg-12 menu">
+			<nav class="col-lg-9">
+				<ul>
+				<li><a href="#">Colletions</a></li>
+				<li><a href="#">Baby</a></li>
+				<li><a href="#">Toddler / Girls</a></li>
+				<li><a href="#">Moms</a></li>
+				<li><a href="#">Accessories</a></li>
+				<li><a href="#">Wedding</a></li>
+				<li><a href="#">Tutumoi Moments</a></li>
+				</ul>
+			</nav>
+			<div class="col-lg-3">
+				<form class="search">
+					<input type="text" value="Search Here"/>
+				</form>
+			</div>
+		</div><!-- end of Menu area -->
+    </header><!-- end of header -->
