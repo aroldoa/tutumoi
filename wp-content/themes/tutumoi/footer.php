8c3a72fcfc83952dc0019d17c0eb37e7b813256f
Aroldo Alvarez <aroldo@primomotif.com>
2014-03-18 15:54:08 -0500

    added header and footer html

--- wp-content/themes/tutumoi/footer.php
+++ wp-content/themes/tutumoi/footer.php
@@ -1,13 +1,44 @@
-		<div id="footer">
-			&copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?>
-		</div>
+    <div class="pre-footer">
+	    <div class="col-lg-12">
 
-	</div>
+	    </div>
+    </div>
+	    <div class="clearfix"></div>
+	<footer>
+	    <div class="col-sm-3">
+		    <h5>Customer Support</h5>
+		    <ul>
+				<li><a href="#">About Us</a></li>
+			    <li><a href="#">Shipping & Returns</a></li>
+			    <li><a href="#">Our  Locations</a></li>
+			    <li><a href="#">Help Placing an Order</a></li>
+		    </ul>
+		</div>
+		<div class="col-sm-3">
+		    <h5>Recent Articles</h5>
+		</div>
+		<div class="col-sm-3">
+		    <h5>Instagram Feed</h5>
+		</div>
+		<div class="col-sm-3">
+		    <h5>Join Our Newsletter</h5>
+		</div>
+	 </footer>
+	 <div class="copyright">
+		 <div class="col-lg-10">
+			 &copy; Copyright 2014 Tutumoi, Inc.
+		 </div>
+		 <div class="col-lg-2"></div>
+	 </div>
+</div><!-- end of Wrap -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="<?php bloginfo( 'template_directory' ); ?>/js/bootstrap.min.js"></script>
 
 	<?php wp_footer(); ?>
-	
 	<!-- Don't forget analytics -->
-	
 </body>
-
 </html>
--- wp-content/themes/tutumoi/header.php
+++ wp-content/themes/tutumoi/header.php
@@ -7,11 +7,9 @@
     <meta name="description" content="">
     <meta name="author" content="">
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-
 	<?php if (is_search()) { ?>
 	   <meta name="robots" content="noindex, nofollow" />
 	<?php } ?>
-
 	<title>
 		   <?php
 		      if (function_exists('is_tag') && is_tag()) {
@@ -32,25 +30,56 @@
 		         echo ' - page '. $paged; }
 		   ?>
 	</title>
-
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
-
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
 	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
+
+	<!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
 	<?php wp_head(); ?>
 
 </head>
 
 <body <?php body_class(); ?>>
-
-	<div id="page-wrap">
-
-		<div id="header">
-			<h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
-			<div class="description"><?php bloginfo('description'); ?></div>
+<div class="container wrap">
+	<div class="col-lg-12 top-links">
+	<div class="col-lg-5"><p>NEED HELP WITH AN ORDER?  INFO@TUTUMOI.COM</p></div>
+		<div class="col-lg-6 col-lg-offset-1">
+			<ul>
+				<a href="#"><li>Sign In</li></a>
+				<a href="#"><li>My Account</li></a>
+				<a href="#"><li>Customer Service</li></a>
+				<a href="#"><li>Shopping Bag</li></a>
+			</ul>
 		</div>
-
+	</div><!-- end of Top Links -->
+	<header>
+		<div class="col-lg-12 logoarea">
+			<a href="#"><img src="<?php bloginfo( 'template_directory' ); ?>/images/logo.png" alt="Tutus"/></a>
+		</div><!-- end of Logo -->
+		<div class="col-lg-12 menu">
+			<nav class="col-lg-9">
+				<ul>
+				<li><a href="#">Colletions</a></li>
+				<li><a href="#">Baby</a></li>
+				<li><a href="#">Toddler / Girls</a></li>
+				<li><a href="#">Moms</a></li>
+				<li><a href="#">Accessories</a></li>
+				<li><a href="#">Wedding</a></li>
+				<li><a href="#">Tutumoi Moments</a></li>
+				</ul>
+			</nav>
+			<div class="col-lg-3">
+				<form class="search">
+					<input type="text" value="Search Here"/>
+				</form>
+			</div>
+		</div><!-- end of Menu area -->
+    </header><!-- end of header -->
