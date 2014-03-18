882471c5add6a138fd1ac35dffec4b94b9706659
Aroldo Alvarez <aroldo@primomotif.com>
2014-03-18 08:41:45 -0500

    update

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
-<?php include('header.php'); ?>
-	<div class="slideshow">
-	    <div class="col-lg-12">
-		    <img class="img-responsive" src="images/slideshow.png" alt="Slideshow"/>
-	    </div>
-    </div><!-- end of slideshow -->
-<?php include('footer.php'); ?>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
---
+++ wp-content/themes/tutumoi/index.php
@@ -0,0 +1,7 @@
+<?php include('header.php'); ?>
+	<div class="slideshow">
+	    <div class="col-lg-12">
+		    <img class="img-responsive" src="images/slideshow.png" alt="Slideshow"/>
+	    </div>
+    </div><!-- end of slideshow -->
+<?php include('footer.php'); ?>
