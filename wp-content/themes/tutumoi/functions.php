93688ac18077759845760afe2b8dd8392dc1fda3
noncasus <noncasus@gmail.com>
2014-03-19 19:35:46 -0500

    Category page edit and menu modification

--- wp-content/themes/tutumoi/functions.php
+++ wp-content/themes/tutumoi/functions.php
@@ -82,4 +82,81 @@
 
         ) );
 
+    function woocommerce_remove_product_thumbnail(){
+        remove_action('woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10 );
+    }
+
+    add_action('woocommerce_before_shop_loop_item_title', 'woocommerce_remove_product_thumbnail');
+
+    function woocommerce_custom_product_category(){
+        global $post;
+        global $product;
+
+        $size = 'shop_catalog';
+        $opening_container = '<div class="overlay">';
+        $attachment_ids = $product->get_gallery_attachment_ids();
+        $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), $size);
+
+        if(has_post_thumbnail() && ($attachment_ids)){
+            $attrs = array(
+                'id'    => 'product_category_image_0',
+                'src'   => $src[0],
+                'class' => "img-responsive attachment-$size",
+                'alt'   => trim(strip_tags( $attachment->post_excerpt )),
+                'title' => trim(strip_tags( $attachment->post_title )),
+            );
+            $img_tags = (get_the_post_thumbnail( $post->ID, $size, $attrs));
+        }
+
+        if($attachment_ids){
+            for($i=0; $i<=0;$i++){
+                $image_link = wp_get_attachment_url( $attachment_ids[$i] );
+                $image_metadata = wp_get_attachment_metadata( $attachment_ids[$i]);
+                if(isset($image_metadata['sizes'])){
+                    $image_size = $image_metadata['sizes'][$size];
+                }
+
+                $img_tags .= '<img id="product_category_image_'. ($i+1) .'" src="' . $image_link . '" width="'. $image_size['width'] .'" height="'. $image_size['height'] .'"  />';
+            }
+            $closing_container = '</div>';
+            $images = $opening_container . $img_tags . $closing_container;
+            echo($images);
+
+        }elseif(has_post_thumbnail()){
+             $attrs = array(
+                'id'    => 'product_category_image_0',
+                'src'   => $src[0],
+                'class' => "img-responsive attachment-$size",
+                'alt'   => trim(strip_tags( $attachment->post_excerpt )),
+                'title' => trim(strip_tags( $attachment->post_title )),
+            );
+            echo (get_the_post_thumbnail( $post->ID, $size, $attrs));
+        }
+        elseif ( wc_placeholder_img_src() )
+            echo (wc_placeholder_img( $size ));
+    }
+
+    add_action( 'woo_custom_product_category', 'woocommerce_custom_product_category' );
+
+
+// ************************************************************************************************************************
+
+    remove_action('woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
+    function woocommerce_custom_price_loop(){
+        global $product;
+
+        if ( $price_html = $product->get_price_html() ) :
+            echo('<div class="prod-price">'.$price_html .'</div>');
+        endif;
+    }
+    add_action( 'woo_custom_price_loop', 'woocommerce_custom_price_loop' );
+
+// ************************************************************************************************************************
+    // function woo_custom_product_loop_start(){
+    //     echo('<div class="col-sm-4">');
+    // }
+    // function woo_custom_product_loop_end(){
+    //     echo('</div>');
+    // }
+
 ?>
